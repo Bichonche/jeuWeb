f1a4355cd143a0af914e1300eaa7d9598388876f
Corentin Normand <perso.corentinnormand@gmail.com>
2015-03-07 02:02:28 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -5,18 +5,27 @@ var count 	= 0;
 var left = [] ;
 var newDiv,cpt2;
 var cpt2 = 0;
+var timer = 10;
 
 
 function myTimer()
 {
+	if (t === 0)
+	{
+		var newDiv = document.createElement('div');
+		document.body.appendChild(newDiv);
+		newDiv.id = 'timer';
+	}
 	if (count <=10)
 	{
-		left[count] = 0;
+		left[count] = 200;
 		newDiv			= document.createElement('div');
-		document.body.appendChild(newDiv);
-		newDiv.className 	= 'redSquare';
+		
 		newDiv.id			= count;
-		newDiv.style.top = (count * 60+20)+'px';
+		newDiv.style.top = (count*60+10)+'px';
+		newDiv.style.left = left[count]+'px';
+		newDiv.className 	= 'redSquare';
+		document.body.appendChild(newDiv);
 		count ++;
 	}
 	if (t >= 400)
@@ -28,7 +37,12 @@ function myTimer()
 			if (left[i]<window.innerWidth-100)
 				document.getElementById(i).style.left = (left[i]+=1)+'px';
 	}
-	if (left[left.length]<window.innerWidth-100)
+	if (t%100 ===0 && t <= 1000)
+	{
+		document.getElementById('timer').innerHTML = timer;
+		timer --;
+	}
+	if (t === 1000)
 			clearInterval(IntervalID);
 		t++;
 }
