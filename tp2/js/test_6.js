1e7624901002c336fbe016e7914eecc6148917e3
Corentin Normand <perso.corentinnormand@gmail.com>
2015-03-16 00:19:55 +0100

    Fichiers finaux pour le tp2 (pièce tournante)

--- tp2/js/test_6.js
+++ tp2/js/test_6.js
@@ -1,28 +1,27 @@
 window.requestAnimationFrame = window.requestAnimationFrame || 	window.mozRequestAnimationFrame ||
                         window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    var canvas = document.getElementById("animatedElem");
-    var i = 0; //compteur de sprites
-    var ctx = canvas.getContext("2d");
-    var image = new Image();
-    var shapes = [];
-    var rotation = 10; //On modifie la vitesse de rotation ici
-    image.src = "coin-sprite-animation.png";
-    image.onload = requestAnimationFrame(step);
+var xCoin,yCoin,x,y;    
+var canvas = document.getElementById("animatedElem");
+var i = 0; //compteur de sprites
+var ctx = canvas.getContext("2d");
+var image = new Image();
+var shapes = [];
+var rotation = 20; //On modifie la vitesse de rotation ici
+image.src = "coin-sprite-animation.png";
+image.onload = requestAnimationFrame(step);
+setInterval(step,1000/rotation-1000/60);
+function step()
+{
+    if(i>=10)
+        i=0;
+    coin(0,0);
+    //requestAnimationFrame(step);
 
-    function step()
-    {
-        if(i>=10)
-            i=0;
-        ctx.clearRect(0,0,100,100);
-        ctx.drawImage(image,100*i,0,100,100,0,0,100,100);
-        ctx.strokeStyle = "#7FFF00";
-        ctx.strokeRect(0,0,100,100); //Obtenir un cadre vert pour la pièce
-        i++;
-        setTimeout(function()
-        {
-            requestAnimationFrame(step);
-        },1000/rotation-1000/60)
+    i++;
 
-    }
-	
-	image.onload = requestAnimationFrame(step);	
+}
+function coin(xCoin,yCoin)
+{
+    ctx.clearRect(xCoin,yCoin,100,100);
+    ctx.drawImage(image,100*i,0,100,100,xCoin,yCoin,100,100);
+}
