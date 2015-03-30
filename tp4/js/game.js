96d87eea7f06de478044c06c4772da293c63e2fc
Corentin Normand <bichonche@gmail.com>
2015-03-30 16:06:21 +0200

    Update game.js
    Ajouter
    
    asteroides
    bonus
    actions de tir
    trajectoires
    fire rate

--- tp4/js/game.js
+++ tp4/js/game.js
@@ -5,12 +5,18 @@ var animFrame = window.requestAnimationFrame ||
             window.msRequestAnimationFrame     ||
             null;
 
+var tics = 0;
+var delayProjectiles = 0;
+var _timeToBeAlive = 30;
+
 //Canvas
 var divArena;
 var canArena;
+var canScore;
 var conArena;
-var arenaWidth = 500;
-var arenaHeight = 300;
+var conScore;
+var arenaWidth = 1000;
+var arenaHeight = 600;
 
 //Background
 var imgBackground;
@@ -18,20 +24,16 @@ var xBackgroundOffset = 0;
 var xBackgroundSpeed = 1;
 var backgroundWidth = 1782;
 var backgroundHeight = 600;
-
-
-//Others
-var i = 0;
-var projectiles = [];
-var iProjectile = 0;
+//une modification
 
 ///////////////////////////////////
 //Keys
 var keys = {
-    UP: 	38,
-    DOWN: 	40,
-    SPACE: 	32,
-    ENTER: 	13
+    UP		: 38,
+    DOWN	: 40,
+    SPACE	: 32,
+    ENTER	: 13,
+    PAUSE	: 80,
 };
 
 var keyStatus = {};
@@ -59,23 +61,309 @@ function keyUpHandler(event) {
 ///////////////////////////////////
 
 
+///////////////////
+// une collection de projectiles
+function ProjectileSet(tabTarget){
+  this.tabTarget = tabTarget;
+  this.score = 0;
+  this.tabProjectiles = new Array();
+  this.add = function (projectile) {
+    this.tabProjectiles.push(projectile);  
+  };
+  this.remove = function () {  
+
+       this.tabProjectiles.map(function(obj,index,array){
+            if(obj.exists == false ||obj.x >arenaWidth || obj.x<0){
+                  delete array[index];
+            }
+        });
+
+  };
+
+
+ this.update = function(){
+        this.remove();
+        var score = 0;
+        this.tabProjectiles.map(function(obj){
+            obj.update();
+            if(obj.exists == false) {//hit
+                score = score +1;
+            }
+        });
+        this.score = this.score + score;
+    };
+ this.clear = function(){
+    this.tabProjectiles.map(function(obj){
+         obj.clear();
+    });
+ };
+ this.draw = function(){
+    this.tabProjectiles.map(function(obj){
+        obj.draw();
+    });
+     //console.log(this.tabProjectiles.length);
+ };
+    
+};
+
+////////////////////
+// un objet Projectile
+function Projectile(x,y,speed,width,height,color){
+    this.x = x;
+    this.y = y;
+    this.xSpeed = speed;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.exists = true;
+    this.collision = function(tabOfObjects){
+        var hits = null;
+        var index;
+        for(index in tabOfObjects){
+            if ((tabOfObjects[index].cptExplosion ==0) && this.x < tabOfObjects[index].x + tabOfObjects[index].width &&
+                this.x + this.width > tabOfObjects[index].x &&
+                this.y < tabOfObjects[index].y + tabOfObjects[index].height &&
+                this.height + this.y > tabOfObjects[index].y) {
+                    // collision detected!
+                    hits = tabOfObjects[index];
+                    break;
+            }
+        }
+        return hits;  
+    };
+    this.draw = function(){
+        if(this.exists){
+            conArena.fillStyle = this.color;
+            conArena.fillRect(this.x,this.y,this.width,this.height);
+        }
+    };
+    this.clear = function(){
+        if(this.exists){
+            conArena.clearRect(this.x-1,this.y-1,this.width+2,this.height+2);
+        }
+    };
+    this.update = function(){
+        if(this.exists){
+            this.x +=   this.xSpeed ;
+            var tmp = this.collision([player].concat(enemies.tabEnemies));
+            if(tmp != null){
+                tmp.explodes();
+                this.exists = false;
+            }
+        }
+    };
+}
+/////////////////////////////////
+
+/////////////////////////////////
+// Enemy
+var enemies = {
+    init : function(){
+        this.tabEnemies = new Array();
+    },
+    add : function (enemy) {
+        this.tabEnemies.push(enemy);  
+    },
+    remove : function () {  
+        this.tabEnemies.map(function(obj,index,array){
+            if(obj.exists == false ||obj.x >arenaWidth || obj.x<0){
+                  delete array[index];
+            }
+        });
+    },
+    draw : function(){ 
+        this.tabEnemies.map(function(obj){
+            obj.draw();
+        });
+    },
+    clear : function(){
+       this.tabEnemies.map(function(obj){
+            obj.clear();
+        });
+    },
+    update : function(){
+
+        this.tabEnemies.map(function(obj){
+            obj.update();
+        });
+         this.remove();
+    }
+    
+};
+//test
+function Enemy(x,y,speed){
+    this.x = x;
+    this.yOrigine = y;
+    this.y = this.yOrigine;
+    this.xSpeed = speed;
+    this.exists = true;
+    this.height = 30;
+    this.width = 40;
+    this.img = new Image();
+    this.img.src = "./assets/Enemy/eSpritesheet_40x30.png";
+    this.cpt = 0;
+
+    this.cptExplosion =  0;//10 images
+    this.imgExplosion = new Image();
+    this.imgExplosionHeight = 128;
+    this.imgExplosionWidth = 128;
+    this.imgExplosion.src = "./assets/Explosion/explosionSpritesheet_1280x128.png";
+
+    this.projectileSet = new ProjectileSet();
+    this.explodes = function(){
+        this.cptExplosion = 1;
+    };
+    this.collision = function(tabOfObjects){
+        var hits = null;
+        var index;
+        for(index in tabOfObjects){
+            if (this.x < tabOfObjects[index].x + tabOfObjects[index].width &&
+                this.x + this.width > tabOfObjects[index].x &&
+                this.y < tabOfObjects[index].y + tabOfObjects[index].height &&
+                this.height + this.y > tabOfObjects[index].y) {
+                    // collision detected!
+                    hits = tabOfObjects[index];
+                    break;
+            }
+        }
+        return hits;
+    };
+    this.fire = function (){
+        var tmp = new Projectile(this.x-10,this.y+this.height/2,-4,10,5,"rgb(0,200,0)");
+        this.projectileSet.add(tmp);
+    };
+    this.draw = function(){ 
+
+        this.projectileSet.draw();
+
+        if(this.cptExplosion!=0){
+                conArena.drawImage(this.imgExplosion, this.cptExplosion*this.imgExplosionWidth, 0, this.imgExplosionWidth,this.imgExplosionHeight, this.x,this.y,this.width,this.height);
+        }else{
+            conArena.drawImage(this.img,  0,this.cpt*this.height,this.width,this.height, this.x,this.y,this.width,this.height);
+        }
+    };
+    this.clear = function(){
+        if(this.exists){
+            conArena.clearRect(this.x,this.y,this.width,this.height);
+        }
+        this.projectileSet.clear();
+    };
+    this.update = function(){
+       if(this.cptExplosion==0){//is not exploding
+            this.x +=   this.xSpeed ;
+            this.y = this.yOrigine+ arenaHeight/3 * Math.sin(this.x / 100);
+            var tmp = this.collision([player]);
+                if(tmp != null){
+                    tmp.explodes();
+                    this.exists = false;
+                }
+
+            if(tics % 5 == 1) {
+                    this.cpt = (this.cpt + 1) % 6;
+            }
+            if(tics % 50 == 1) this.fire();
+       }else{
+            if(tics % 3 == 1) {
+                this.cptExplosion++;
+            }
+            if(this.cptExplosion>10){//end of animation
+                this.cptExplosion=0;
+                this.exists = false;
+            }
+        }
+        this.projectileSet.update();
+    };
+}
+/////////////////////////////////
 
 /////////////////////////////////
 // Hero Player
-var imgPlayer 		= new Image();
-imgPlayer.src 		= "./assets/Ship/player.png";
-var imgProjectile	= new Image();
-imgProjectile.src	= "./assets/Ship/fireballs.png";
-var projectileImgHeight= 16;
-var projectileImgWidth = 16;
-var xPlayer 		= 20;
-var yPlayerSpeed 	= 10;
-var yPlayer 		= 100;
-var playerHeight 	= 15;
-var playerWidth 	= 32;
-var playerImgHeight = 29;
-var playerImgWidth 	= 64;
-/////////////////////////////////
+var player = {
+    init : function(){
+        this.img = new Image();
+        this.img.src = "./assets/Ship/Spritesheet_64x29.png";
+        this.cpt = 0;
+        this.cptExplosion =  10;//10 images
+        this.imgExplosion = new Image();
+        this.imgExplosionHeight = 128;
+        this.imgExplosionWidth = 128;
+        this.imgExplosion.src = "./assets/Explosion/explosionSpritesheet_1280x128.png";
+        this.projectileSet = new ProjectileSet();
+    },
+    x : 20,
+    ySpeed : 5,
+    y : 100,
+    height : 29,
+    width : 64,
+    nbOfLives : 2,
+    timeToBeAlive : 0,
+    fires : function(){
+        var tmp = new Projectile(this.x+this.width,this.y+this.height/2,4,10,3,"rgb(200,0,0)");
+        this.projectileSet.add(tmp);
+    },
+    explodes : function(){
+        if(this.timeToBeAlive == 0) {
+            this.nbOfLives--;
+            if(this.nbOfLives>0){
+                this.timeToBeAlive = _timeToBeAlive;
+                this.cptExplosion = 1;
+            }else{
+                //Game Over
+                console.log("GAME OVER");
+            }
+        }
+    },
+    clear : function(){
+        conArena.clearRect(this.x,this.y,this.width,this.height);
+        this.projectileSet.clear();
+    },
+    update :  function(){
+        var keycode;
+        if(tics % 10 == 1) {
+                this.cpt = (this.cpt + 1) % 4;
+            }
+        if(this.timeToBeAlive>0) {
+            this.timeToBeAlive --;
+        }else{
+            for (keycode in keyStatus) {
+                if(keyStatus[keycode] == true){
+                    if(keycode == keys.UP) {
+                        this.y -= this.ySpeed;
+                        if(this.y<0) this.y=0;
+                    }
+                    if(keycode == keys.DOWN) {
+                        this.y += this.ySpeed;
+                        if(this.y>arenaHeight-this.height) this.y=arenaHeight-this.height;
+                    }
+                    if(keycode == keys.SPACE) {
+                        //shoot
+                        //window.setTimeout(this.fires,200);
+                    	if (delayProjectiles % 20 === 0) {this.fires();}
+                    	delayProjectiles++;
+                    	console.log(delayProjectiles);
+                    }
+                    delayProjectiles = 0;
+                }
+             //keyStatus[keycode] = false;
+            }
+        }
+        this.projectileSet.update();
+    },
+    draw : function(){
+        if(this.timeToBeAlive == 0) {
+
+            conArena.drawImage(this.img, 0,this.cpt*this.height,this.width,this.height, this.x,this.y,this.width,this.height);
+        }else{
+            //exploding
+            if(this.cptExplosion!=0){
+                conArena.drawImage(this.imgExplosion, this.cptExplosion*this.imgExplosionWidth, 0, this.imgExplosionWidth,this.imgExplosionHeight, this.x,this.y,this.width,this.height);
+               if(tics % 3 == 1) {this.cptExplosion++;}
+                if(this.cptExplosion>10) this.cptExplosion=0;
+            }
+        }
+        this.projectileSet.draw();
+    }
+};
 
 
 
@@ -85,80 +373,65 @@ function updateScene() {
 }
 function updateItems() {
     "use strict"; 
-    clearItems();
-    
-    var keycode;
-    for (keycode in keyStatus) {
-            if(keyStatus[keycode] == true){
-                if(keycode == keys.UP) {
-                	if (yPlayer > 0) { 
-                    	yPlayer -= yPlayerSpeed;   
-                    }
-                }
-                if(keycode == keys.DOWN) { 
-                	if (yPlayer < arenaHeight - playerImgHeight) {
-                    	yPlayer += yPlayerSpeed;
-                    }   
-                } 
-                if(keycode == keys.SPACE ) { 
-                    shootProjectile();
-                }             
-            }
-        keyStatus[keycode] = false;
+    player.update();
+    tics++;
+
+     if(tics % 100 == 1) {
+         var rand = Math.floor(Math.random() * arenaHeight);
+
+        enemies.add(new Enemy(arenaWidth, rand,-2));
     }
+    enemies.update();
 }
 function drawScene() {
     "use strict"; 
     canArena.style.backgroundPosition = xBackgroundOffset + "px 0px" ;
 }
 function drawItems() {
-	if (i > 3) {i = 0;}
     "use strict"; 
-    conArena.drawImage(imgPlayer, 0, playerImgHeight*i, playerImgWidth, playerImgHeight , xPlayer, yPlayer, playerWidth, playerHeight);
-
-    //requestAnimationFrame(drawItems, 1000);
-    for (var cpt = 0 ; cpt < projectiles.length ; cpt ++) {
-    	projectiles[cpt][0] += 10;
-    	if (projectiles[cpt][0] >10) {projectiles.splice(cpt, 0);}
-    	conArena.drawImage(imgProjectile, projectileImgHeight * i + 4, 0, projectileImgWidth, projectileImgHeight, projectiles[cpt][0], projectiles[cpt][1], 10, 10);	
-   	}
-   	i++;
+    player.draw();
+    enemies.draw();
 }
 function clearItems() {
     "use strict"; 
-    conArena.clearRect(xPlayer,yPlayer,playerWidth,playerHeight);
-    for (var cpt = 0 ; cpt < projectiles.length ; cpt ++) {
-    	conArena.clearRect(projectiles[cpt][0], projectiles[cpt][1], projectileImgWidth, projectileImgHeight);
-   	}
+    player.clear(); 
+    enemies.clear();
 }
 
+function clearScore() {
+    conScore.clearRect(0,0,300,50);
+}
+function drawScore() {
+	conScore.fillText("Lives : "+player.nbOfLives, 10, 25);
+	conScore.fillText("Score : "+player.projectileSet.score, 150,25);
+}
+
+function gameOver() {
+	clearGame();
+	conScore.fillText("Game Over", 300,500);
+}
 function updateGame() {
     "use strict"; 
     updateScene();
     updateItems();
 }
+function clearGame() {
+    "use strict"; 
+    clearItems();
+    clearScore();
+}
 
 function drawGame() {
     "use strict"; 
     drawScene();
+    drawScore();
     drawItems();    
 }
 
-function shootProjectile() {
-	var xProjectile, yProjectile;
-	xProjectile = xPlayer + playerWidth;
-	yProjectile = yPlayer + playerHeight/2;
-	projectiles.push([xProjectile, yProjectile]);
-	/*for (var cptTmp = 0 ; cptTmp < projectiles.length ; cptTmp ++ ) {
-		projectiles[cptTmp][0] += 10;
-	}
-	*/	
-}
-////////////////////////////
-//Main Loop
 
 function mainloop () {
     "use strict"; 
+    clearGame();
     updateGame();
     drawGame();
 }
@@ -174,11 +447,23 @@ function init() {
     divArena = document.getElementById("arena");
     canArena = document.createElement("canvas");
     canArena.setAttribute("id", "canArena");
+    canArena.setAttribute("height", arenaHeight);
+    canArena.setAttribute("width", arenaWidth);
     conArena = canArena.getContext("2d");
-    canArena.setAttribute("height",arenaHeight);
-    canArena.setAttribute("width",arenaWidth);
     divArena.appendChild(canArena);
+
+    canScore = document.createElement("canvas");
+    canScore.setAttribute("id","canScore");
+    canScore.setAttribute("height", arenaHeight);
+    canScore.setAttribute("width", arenaWidth);
+    conScore = canScore.getContext("2d");
+    conScore.fillStyle = "rgb(200,0,0)";
+    conScore.font = 'bold 12pt Courier';
+    divArena.appendChild(canScore);
+
  
+    player.init();
+    enemies.init();
     
 window.addEventListener("keydown", keyDownHandler, false);
 window.addEventListener("keyup", keyUpHandler, false);
